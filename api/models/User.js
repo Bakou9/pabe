75505d3f75de45b940e4fcb576af33bc9940c5fc
Bakou <banjax_rbannes@hotmail.com>
2017-04-09 11:21:50 +0200

    Add user api CRUD

---
+++ api/models/User.js
@@ -0,0 +1,21 @@
+/**
+ * User.js
+ *
+ * @description :: Users of webSite.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
